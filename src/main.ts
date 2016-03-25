3f9e187e6e4fce9fad2865bb224a01fc97ed5863
robinryf <robinryf@gmail.com>
2016-03-25 23:27:02 +0100

    -MOVED Files to src dir

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,8 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
 
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
-
-
+  console.log("Base dir: " + __dirname);
+  
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   mainWindow.on('closed', function() {
