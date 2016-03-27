e638009c1239768118516770001633a197dd8bcd
robinryf <robinryf@gmail.com>
2016-03-27 20:15:06 +0200

    -MODIFIED Streaming build system is implemented and working.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var tsb = require('gulp-tsb');
+var watch = require('gulp-watch');
 	
 	// create and keep compiler 
 	var compilation = tsb.create({
@@ -7,17 +8,44 @@ var tsb = require('gulp-tsb');
 		module: 'commonjs',
 		declaration: false
 	});
-	
-	gulp.task('compile', function() {
-		return gulp.src('src/**/*.ts')
-			.pipe(compilation())
-			.pipe(gulp.dest('./dist/local'));
-	});
     
-    gulp.task('copy-index-html', function() {
-    gulp.src('./src/**/*.html')
-    // Perform minification tasks, etc here
-    .pipe(gulp.dest('./dist/local'));
+    var srcGlob = 'src/**/*.ts';
+    var htmlGlob = './src/**/*.html';
+    var dest = './dist/local';
+    
+
+    gulp.task('stream-compile', function ()
+    {
+        watch(srcGlob, function()
+        {
+           gulp.start('compile');
+        });
     });
     
-gulp.task('default', ['compile', 'copy-index-html']);
+    gulp.task('stream-html', function () 
+    {
+        watch(htmlGlob, function ()
+        {
+           gulp.start('copy-html')
+        });
+    });
+    
+	
+    // DEFAULT
+    gulp.task('compile', function() 
+    {
+		return gulp.src(srcGlob)
+			.pipe(compilation())
+			.pipe(gulp.dest(dest));
+	});
+    
+    gulp.task('copy-html', function() 
+    {
+         return gulp.src(htmlGlob)
+            .pipe(gulp.dest(dest));
+    });
+    
+    
+    
+gulp.task('default', ['compile', 'copy-html']);
+gulp.task('watch', ['stream-compile', 'stream-html']);
