5959d8efee287878732bd2f8315028f410383932
robinryf <robinryf@gmail.com>
2016-03-28 12:46:34 +0200

    -ADDED some typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="browser/ambient/github-electron/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
+/// <reference path="browser/ambient/three/index.d.ts" />
+/// <reference path="browser/ambient/threejs/detector/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="main/ambient/github-electron/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
+/// <reference path="main/ambient/three/index.d.ts" />
+/// <reference path="main/ambient/threejs/detector/index.d.ts" />
